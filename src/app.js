ebc5dd418a1839e726b7afef98f0e6d605892c3a
Dharmendra Pandit <dharmendra763297@gmail.com>
2023-11-14 23:55:16 +0530

    port

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express")
-const path  = require("path")
+const path = require("path")
 const app = express();
 const hbs = require('hbs');
-const port =  process.env.PORT || 5000
+// const port =  process.env.PORT || 5000
 
-const pathName = path.join(__dirname,"../public")
-const template_path = path.join(__dirname,"../src/template/views")
-const partials_path = path.join(__dirname,"../src/template/partials")
+const port = 5000;
+
+const pathName = path.join(__dirname, "../public")
+const template_path = path.join(__dirname, "../src/template/views")
+const partials_path = path.join(__dirname, "../src/template/partials")
 
 console.log(`path name public ${pathName}`)
 console.log(`template_path name public ${template_path}`)
@@ -15,24 +17,25 @@ app.set('view engine', 'hbs');
 app.set('views', template_path);
 app.use(express.static(pathName));
 hbs.registerPartials(partials_path);
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index')
 })
-app.get("/index",(req,res)=>{
+app.get("/index", (req, res) => {
     res.render('index')
 })
-app.get("/weather",(req,res)=>{
+app.get("/weather", (req, res) => {
     res.render("weather")
 })
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     // res.send("about page")
     res.render('about')
 })
-app.get("*",(req,res)=>{
+app.get("*", (req, res) => {
     res.render("errorPage")
 })
 
-app.listen(port,()=>{
- console.log(`Website is live at post ${port}`)
-})
+// app.listen(port,()=>{
+//  console.log(`Website is live at post ${port}`)
+// })
 // 
+app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}`))
