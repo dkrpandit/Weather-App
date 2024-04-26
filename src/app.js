603c04e4037baa84f3c467a35a0323fdb005bd92
Dharmendra Pandit <99905700+dkrpandit@users.noreply.github.com>
2024-04-26 10:45:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require("express")
 const path = require("path")
 const app = express();
 const hbs = require('hbs');
-// const port =  process.env.PORT || 5000
+// const port =  process.env.PORT || 3000
 
-const port = 5000;
+const port = 3000;
 
 const pathName = path.join(__dirname, "../public")
 const template_path = path.join(__dirname, "../src/template/views")
